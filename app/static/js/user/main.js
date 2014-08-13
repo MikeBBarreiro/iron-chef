f772ff4327975c0475ea56dfbb0de855fe261816
Michael Barreiro <Mikeb_barreiro@yahoo.com>
2014-08-13 10:35:10 -0500

    included new button with css to form

--- app/static/js/user/main.js
+++ app/static/js/user/main.js
@@ -3,6 +3,8 @@
 
   $(document).ready(function(){
     $('form').submit(addRecipe);
+    $('.blue').click(hide);
+    $('#show').click(show);
   });
   function addRecipe(e){
     console.log('adding new recipe');
@@ -11,13 +13,25 @@
         type = $('form').attr('method'),
         url = $('form').attr('action');
 
+
     $.ajax({url:url, type:type, data:data, dataType:'html', success:function(html){
-      $('#recipes').prepend(html);
+//Allows the recipes to fade in!
+      var $recipe = $(html);
+      $recipe.css('display', 'none');
+      $('#recipes').prepend($recipe);
+      $recipe.fadeIn(2000);
     }});
     console.log(data, type, url);
 
     e.preventDefault();
   }
+
+  function hide(){
+    $('form').fadeOut();
+}
+  function show(){
+    $('form').fadeIn();
+}
 
 })();
 
